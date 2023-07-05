558f29f791ebbe38f9ab5c154dd54262e7327730
Matthias Kleine <github.com9@emailu.de>
2023-07-05 12:21:45 +0200

    Copied table component (ConfigTable) as base

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -1,24 +1,91 @@
-import { useState } from 'react'
+// this file used only for simulation and not used in end build
 
-function App() {
-  const [count, setCount] = useState(0)
+import React from 'react';
+import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
 
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import withStyles from '@mui/styles/withStyles';
+
+import GenericApp from '@iobroker/adapter-react-v5/GenericApp';
+import I18n from '@iobroker/adapter-react-v5/i18n';
+import Loader from '@iobroker/adapter-react-v5/Components/Loader';
+
+import AwtrixAppComponent from './AwtrixAppComponent';
+
+const styles = theme => ({
+    app: {
+        backgroundColor: theme.palette.background.default,
+        color: theme.palette.text.primary,
+        height: '100%',
+    },
+    item: {
+        padding: 50,
+        width: 400
+    }
+});
+
+class App extends GenericApp {
+    constructor(props) {
+        const extendedProps = { ...props };
+        super(props, extendedProps);
+
+        this.state = {
+            data: { myCustomAttribute: 'red' },
+            theme: this.createTheme(),
+        };
+        /*
+        const translations = {
+            en: require('./i18n/en.js'),
+            de: require('./i18n/de.js'),
+            ru: require('./i18n/ru.js'),
+            pt: require('./i18n/pt.js'),
+            nl: require('./i18n/nl.js'),
+            fr: require('./i18n/fr.js'),
+            it: require('./i18n/it.js'),
+            es: require('./i18n/es.js'),
+            pl: require('./i18n/pl.js'),
+            uk: require('./i18n/uk.js'),
+            'zh-cn': require('./i18n/zh-cn').json,
+        };
+
+        I18n.setTranslations(translations);
+        */
+        I18n.setLanguage((navigator.language || navigator.userLanguage || 'en').substring(0, 2).toLowerCase());
+    }
+
+    render() {
+        if (!this.state.loaded) {
+            return <StyledEngineProvider injectFirst>
+                <ThemeProvider theme={this.state.theme}>
+                    <Loader theme={this.state.themeType} />
+                </ThemeProvider>
+            </StyledEngineProvider>;
+        }
+
+        return <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={this.state.theme}>
+                <div className={this.props.classes.app}>
+                    <div className={this.props.classes.item}>
+                        <AwtrixAppComponent
+                            socket={this.socket}
+                            themeType={this.state.themeType}
+                            themeName={this.state.themeName}
+                            attr='historyApps'
+                            data={this.state.data}
+                            onError={() => {}}
+                            instance={0}
+                            schema={{
+                                name: 'ConfigCustomAwtrixSet/Components/AwtrixAppComponent',
+                                type: 'custom',
+                            }}
+                            onChange={data => {
+                                this.setState({ data });
+                            }}
+                        />
+                    </div>
+                </div>
+            </ThemeProvider>
+        </StyledEngineProvider>;
+    }
 }
 
-export default App
+export default withStyles(styles)(App);
--- src/src/main.jsx
+++ src/src/main.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
+import Utils from '@iobroker/adapter-react-v5/Components/Utils';
+import App from './App'
+import theme from './theme';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+window.adapterName = 'adapter-component-template';
+let themeName = Utils.getThemeName();
+
+function build() {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <StyledEngineProvider injectFirst>
+              <ThemeProvider theme={theme(themeName)}>
+                  <App
+                      socket={{port: 8081}}
+                      onThemeChange={(_theme) => {
+                          themeName = _theme;
+                          build();
+                      }}
+                  />
+              </ThemeProvider>
+          </StyledEngineProvider>
+    </React.StrictMode>
+  );
+}
+
+build();
