5430b494209ccb002d293fb03439e50b9ea71437
Matthias Kleine <github.com9@emailu.de>
2023-10-01 12:17:10 +0200

    Use axios instance

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
 import { AwtrixLight } from '../main';
 
 export namespace AwtrixApi {
@@ -52,35 +52,32 @@ export namespace AwtrixApi {
 
     export class Client {
         private adapter: AwtrixLight;
-
-        private ipAddress: string;
-        private port: number;
-        private httpTimeout: number;
-
-        private apiConnected: boolean;
-        private auth: axios.AxiosBasicCredentials | undefined;
-
-        private lastErrorCode: number;
+        private axiosInstance: AxiosInstance | undefined = undefined;
+        private apiConnected: boolean = false;
+        private lastErrorCode: number = -1;
 
         public constructor(adapter: AwtrixLight, ipAddress: string, port: number, httpTimeout: number, userName: string, userPassword: string) {
             this.adapter = adapter;
 
-            this.ipAddress = ipAddress;
-            this.port = port;
-            this.httpTimeout = httpTimeout;
+            this.adapter.log.info(`Starting - connecting to http://${ipAddress}:${port}/`);
 
-            this.adapter.log.info(`Starting - connecting to http://${this.ipAddress}:${this.port}/`);
-
-            this.apiConnected = false;
-
+            let httpAuth: axios.AxiosBasicCredentials | undefined = undefined;
             if (userName) {
-                this.auth = {
+                httpAuth = {
                     username: userName,
                     password: userPassword,
                 };
             }
 
-            this.lastErrorCode = -1;
+            this.axiosInstance = axios.create({
+                baseURL: `http://${ipAddress}:${port}/api/`,
+                timeout: httpTimeout * 1000 || 3000,
+                auth: httpAuth,
+                validateStatus: (status) => {
+                    return [200, 201].indexOf(status) > -1;
+                },
+                responseType: 'json',
+            });
         }
 
         public isConnected(): boolean {
@@ -132,69 +129,52 @@ export namespace AwtrixApi {
             return this.requestAsync(`custom?name=${name}`, 'POST', data);
         }
 
-        public async requestAsync(service: string, method?: string, data?: object): Promise<AxiosResponse> {
+        public async requestAsync(url: string, method?: string, data?: object): Promise<AxiosResponse> {
             return new Promise<AxiosResponse>((resolve, reject) => {
-                const url = `/api/${service}`;
-                const timeout = this.httpTimeout * 1000 || 3000;
 
-                if (this.ipAddress) {
-                    if (data) {
-                        this.adapter.log.debug(`sending "${method}" request to "${url}" with data: ${JSON.stringify(data)}`);
-                    } else {
-                        this.adapter.log.debug(`sending "${method}" request to "${url}" without data`);
-                    }
-
-                    axios({
-                        method,
-                        data,
-                        baseURL: `http://${this.ipAddress}:${this.port}`,
-                        url,
-                        timeout,
-                        auth: this.auth,
-                        validateStatus: (status) => {
-                            return [200, 201].indexOf(status) > -1;
-                        },
-                        responseType: 'json',
-                    })
-                        .then((response) => {
-                            this.adapter.log.debug(`received ${response.status} response from "${url}" with content: ${JSON.stringify(response.data)}`);
-
-                            // no error - clear up reminder
-                            this.lastErrorCode = -1;
-
-                            resolve(response);
-                        })
-                        .catch((error) => {
-                            if (error.response) {
-                                // The request was made and the server responded with a status code
-
-                                if (error.response.status === 401) {
-                                    this.adapter.log.warn('Unable to perform request. Looks like the device is protected with username / password. Check instance configuration!');
-                                } else {
-                                    this.adapter.log.warn(`received ${error.response.status} response from ${url} with content: ${JSON.stringify(error.response.data)}`);
-                                }
-                            } else if (error.request) {
-                                // The request was made but no response was received
-                                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-                                // http.ClientRequest in node.js
-
-                                // avoid spamming of the same error when stuck in a reconnection loop
-                                if (error.code === this.lastErrorCode) {
-                                    this.adapter.log.debug(error.message);
-                                } else {
-                                    this.adapter.log.info(`error ${error.code} from ${url}: ${error.message}`);
-                                    this.lastErrorCode = error.code;
-                                }
-                            } else {
-                                // Something happened in setting up the request that triggered an Error
-                                this.adapter.log.error(error.message);
-                            }
-
-                            reject(error);
-                        });
+                if (data) {
+                    this.adapter.log.debug(`sending "${method}" request to "${url}" with data: ${JSON.stringify(data)}`);
                 } else {
-                    reject('Device IP is not configured');
+                    this.adapter.log.debug(`sending "${method}" request to "${url}" without data`);
                 }
+
+                this.axiosInstance!.request({ url, method, data })
+                    .then((response) => {
+                        this.adapter.log.debug(`received ${response.status} response from "${url}" with content: ${JSON.stringify(response.data)}`);
+
+                        // no error - clear up reminder
+                        this.lastErrorCode = -1;
+
+                        resolve(response);
+                    })
+                    .catch((error) => {
+                        if (error.response) {
+                            // The request was made and the server responded with a status code
+
+                            if (error.response.status === 401) {
+                                this.adapter.log.warn('Unable to perform request. Looks like the device is protected with username / password. Check instance configuration!');
+                            } else {
+                                this.adapter.log.warn(`received ${error.response.status} response from ${url} with content: ${JSON.stringify(error.response.data)}`);
+                            }
+                        } else if (error.request) {
+                            // The request was made but no response was received
+                            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                            // http.ClientRequest in node.js
+
+                            // avoid spamming of the same error when stuck in a reconnection loop
+                            if (error.code === this.lastErrorCode) {
+                                this.adapter.log.debug(error.message);
+                            } else {
+                                this.adapter.log.info(`error ${error.code} from ${url}: ${error.message}`);
+                                this.lastErrorCode = error.code;
+                            }
+                        } else {
+                            // Something happened in setting up the request that triggered an Error
+                            this.adapter.log.error(error.message);
+                        }
+
+                        reject(error);
+                    });
             });
         }
     }
