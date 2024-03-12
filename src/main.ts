6fdd9351b36cff3e1bd26563a9e29abc8a62fbae
Matthias Kleine <github.com9@emailu.de>
2024-03-12 11:01:51 +0100

    Updated recommended firmware version to 0.95

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ export class AwtrixLight extends utils.Adapter {
             useFormatDate: true,
         });
 
-        this.supportedVersion = '0.94';
+        this.supportedVersion = '0.95';
         this.displayedVersionWarning = false;
 
         this.apiClient = null;
