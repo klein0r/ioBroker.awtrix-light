85dd0f728954a44b769308df709c29146c673794
Matthias Kleine <github.com9@emailu.de>
2025-07-02 09:06:13 +0200

    Removed useless test

--- src/main.test.ts
+++ src/main.test.ts
@@ -5,21 +5,6 @@
  * It is advised to test all your modules with accompanying *.test.ts-files
  */
 
-import { expect } from 'chai';
 // import { functionToTest } from "./moduleToTest";
-
-describe('module to test => function to test', () => {
-    // initializing logic
-    const expected = 5;
-
-    it(`should return ${expected}`, () => {
-        const result = 5;
-        // assign result a value from functionToTest
-        expect(result).to.equal(expected);
-        // or using the should() syntax
-        result.should.equal(expected);
-    });
-    // ... more tests => it
-});
 
 // ... more test suites => describe
