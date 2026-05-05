127dc0c03ae3ec49c4914101d1ad76b3fc5b6be3
Matthias Kleine <github.com9@emailu.de>
2026-05-05 16:27:27 +0200

    Migrated to @iobroker/eslint-config

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import config from '@iobroker/eslint-config';
+
+export default [
+    ...config,
+    {
+        ignores: [
+            '.dev-server/',
+            '.vscode/',
+            '*.test.js',
+            'test/**/*.js',
+            '*.config.mjs',
+            'build',
+            'dist',
+            'admin/build', 
+            'admin/words.js',
+            'admin/admin.d.ts',
+            'admin/blockly.js',
+            '**/adapter-config.d.ts',
+        ],
+    },
+    {
+        rules: {
+            'jsdoc/require-jsdoc': 'off',
+            'jsdoc/require-param': 'off',
+            'jsdoc/require-param-description': 'off',
+            'jsdoc/require-returns-description': 'off',
+            'jsdoc/require-returns-check': 'off',
+        },
+    },
+];
